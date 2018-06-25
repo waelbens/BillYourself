a75bec3a2304bb5d5581281195d0b661d5bd78bf
waelbens <wael.ben.salah@gmail.com>
2018-06-25 20:04:12 +0200

    Add UserComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { SigninComponent } from './user/signin/signin.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { BillResultsComponent } from './bill/bill-results/bill-results.component
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
     SignupComponent,
     SigninComponent,
     BillComponent,
